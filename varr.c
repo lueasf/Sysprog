19eedd227e20ccd6e8be2fbc6b8a19882dc7d3da
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2024-09-13 15:32:54 +0200

    sys

--- varr.c
+++ varr.c
@@ -11,4 +11,16 @@ int main(){
         setenv("COOLVAR", "cool value", 1);
         printf("La variable $COOLVAR n'existe pas\n");
     }
+
+    // 2
+    int ret = setenv("MAVAR", "picasso", 0); // 0 pour remplacer
+    if (ret == 0){
+        printf("$MAVAR : %s\n", getenv("MAVAR"));
+    }
+    unsetenv("MAVAR");
+    char *v = getenv("MAVAR");
+    if (v == NULL){
+        printf("$MAVAR nexiste plus.\n");
+    }
+    return 0;
 }
